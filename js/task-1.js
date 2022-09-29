4e1f3414ff0526d9b1a671640f62ca6aada35569
ItSites <front3@itsites.com.ua>
2022-09-29 17:05:22 +0300

    fix

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
 const inpNumber = +prompt('Введіть число !!!');
 let s = 0;
 
-for (let i = 1; i!==inpNumber;i+=1) {
+for (let i = 0; i!==inpNumber+1;i+=1) {
     if (inpNumber % i === 0) {
         s+=i;
     }
